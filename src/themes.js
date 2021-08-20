b691fdb6a3e8495b7736697d2c0f3b81b27328ce
jesus30000 <jesusochoagonzalez@gmail.com>
2021-08-19 18:22:15 -0600

    7 files added, 12 deleted

---
+++ src/themes.js
@@ -0,0 +1,10 @@
+const theme = {
+  fondo: '#F9F9F9',
+  colorPrimario: '#ff90b5',
+  colorPrimarioHover: '#e86c95',
+  colorSecundario: '#ff90b552',
+  verde: '#43A854',
+  rojo: '#E34747'
+}
+
+export default theme;
